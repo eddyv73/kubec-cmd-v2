89c624fd13370a7b76d4c7a495a61f3152f131b1
Wister <e@wister.mx>
2024-05-28 03:20:33 -0600

    yaml config init

---
+++ kubec-cmd/Commons/Config-Kubec.cs
@@ -0,0 +1,16 @@
+﻿namespace kubec_cmd.Commons;
+
+public class Config_Kubec
+{
+    public string CurrentConfig { get; set; }
+    public string backupDirectory { get; set; }
+    ListFiles[] files { get; set; }
+    
+}
+
+
+public class ListFiles
+{
+    public string fileName { get; set; }
+    public string Dir { get; set; }
+}
